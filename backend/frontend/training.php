25fef358de12c70df0c2dcfd9165ea61c2677e09
Christoper Luis Alexander <59040138+Christoper0704@users.noreply.github.com>
2021-08-13 17:33:44 +0700

    Update training.php

--- backend/frontend/training.php
+++ backend/frontend/training.php
@@ -40,14 +40,45 @@
 </nav>
 
 <div class="container mt-5">
-  <form class="form-inline" action="datatraining.php">
-	<label for="name" class="mr-sm-2 text-dark">Name : </label>
-	<input class="form-control mb-2 mr-sm-2 mt-2" type="text" id="name" name="name" placeholder="Input Name">
-	<label for="role" class="mr-sm-2 text-dark">Role : </label>
-	<input class="form-control mb-2 mr-sm-2 mt-2" type="text" id="role" name="role" placeholder="Input role">
-	<br>
-	<button type="submit" class="btn btn-success mb-2 mt-2">Start</button>
-	<button type="reset" class="btn btn-danger ml-2 mb-2 mt-2">Reset</button>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method = "post">
+  <div class="form-row <?php echo (!empty($firstname_err)) ? 'has-error' : ''; ?>">
+	  <div class="col">
+		<label for="firstname" class="mb-2 mr-sm-2 mt-2 text-dark">Firstname : </label>
+	  </div>
+	  <div class="col">
+		<input class="form-control mb-2 mr-sm-2 mt-2" type="text" value="<?php echo $firstname; ?>" name="firstname" placeholder="Input First Name">
+		<span class="help-block"><?php echo $firstname_err ?></span>
+	  </div>
+  </div>
+  <div class="form-row <?php echo (!empty($lastname_err)) ? 'has-error' : ''; ?>">
+	<div class="col">
+		<label for="lastname" class="mb-2 mr-sm-2 mt-2 text-dark">Lastname : </label>
+	</div>
+	<div class="col">
+		<input class="form-control mb-2 mr-sm-2 mt-2" type="text" value="<?php echo $lastname; ?>" name="lastname" placeholder="Input Last Name">
+		<span class="help-block"><?php echo $lastname_err ?></span>
+	</div>
+  </div>
+  <div class="form-row <?php echo (!empty($gender_err)) ? 'has-error' : ''; ?>">
+	<div class="col">
+		<label for="gender" class="mb-2 mr-sm-2 mt-2 text-dark">Gender : </label>
+	</div>
+	<div class="col">
+		<input class="form-control mb-2 mr-sm-2 mt-2" type="text" value="<?php echo $gender; ?>" name="gender" placeholder="Input Gender">
+		<span class="help-block"><?php echo $gender_err ?></span>
+	</div>
+  </div>
+  <div class="form-row <?php echo (!empty($role_err)) ? 'has-error' : ''; ?>">
+	<div class="col">
+		<label for="role" class="mb-2 mr-sm-2 mt-2 text-dark">Role : </label>
+	</div>
+	<div class="col">
+		<input class="form-control mb-2 mr-sm-2 mt-2" type="text" value="<?php echo $role; ?>" name="role" placeholder="Input Role">
+		<span class="help-block"><?php echo $role_err ?></span>
+	</div>
+  </div>
+  <button type="submit" class="btn btn-success mb-2 mt-2">Start</button>
+  <button type="reset" class="btn btn-danger ml-2 mb-2 mt-2">Reset</button>
   </form>
 </div>
 
