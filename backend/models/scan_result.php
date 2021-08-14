589167b5a2397bc4ccbed23145020c1950e42800
Christoper Luis Alexander <59040138+Christoper0704@users.noreply.github.com>
2021-08-14 19:53:29 +0700

    Update scan_result.php

--- backend/models/scan_result.php
+++ backend/models/scan_result.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+$loggedin = "";
+
 if(isset($_SESSION["loggedin"]) == FALSE){
 	header("Location: ../index.php");
 	exit;
@@ -9,17 +11,48 @@ if(isset($_SESSION["loggedin"]) == FALSE){
 require_once "./functions.php";
 ?>
 
-<html>
-	<head>
-		<title>Scan Result</title>
-	</head>
-<style>
-table, th, td {
-	border: 1px solid black;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+<title>e-Eyes</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width-device-width initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../styles/style1.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script src="../styles/index.js"></script>
+</head>
+
 <body>
-<a href="../index.php">Go back</a>
+<nav class="navbar navbar-expand-sm bg-info navbar-dark navbar-static-top">
+<a class="navbar-brand" href="../index.php">e-Eyes</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="../index.php">HOME</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="../about.php">ABOUT</a>
+</li>
+</ul>
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" href="../auth/logout.php"><img src="../images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
+</li>
+</ul>
+</div>
+</nav>
+
+<body>
 
 <?php
 
@@ -43,7 +76,11 @@ $stmt = $conn->query($query);
 
 if($stmt->num_rows > 0){
     echo <<< EOT
-        <table>
+        <div class="container mt-5">
+            <div class="table-responsive-sm">    
+            <h3 center class="text-dark">SCAN RESULT</h3>
+        <table class="table table-bordered">
+        <thead>
             <tr>
                 <th>No</th>
                 <th>Photo</th>
@@ -53,6 +90,8 @@ if($stmt->num_rows > 0){
                 <th>Scan Date</th>
                 <th>Temperature</th>
             </tr>
+        </thead>
+        <tbody>
     EOT;
 
     $i = 0;
@@ -77,11 +116,17 @@ if($stmt->num_rows > 0){
             <tr>";
     
     }
-    echo "</table>";
+    echo "</tbody></table>";
 }else{
     echo "0 results";
 }
 
 ?>
+</div></div>
+
+<div class="jumbotron text-center text-white bg-dark" style="margin-bottom:0">
+  <p>Copyright ©2021</p>
+</div>
+
 </body>
 </html>
