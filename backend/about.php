52448b47f4432bbcf29d615300f5f38a871c3d7b
Christoper Luis Alexander <59040138+Christoper0704@users.noreply.github.com>
2021-08-14 19:51:12 +0700

    Update about.php

--- backend/about.php
+++ backend/about.php
@@ -7,33 +7,33 @@
 <meta name="viewport" content="width-device-width initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/style1.css">
+<link rel="stylesheet" href="./styles/style1.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-<script src="js/index.js"></script>
+<script src="./styles/index.js"></script>
 </head>
 
 <body>
 <nav class="navbar navbar-expand-sm bg-info navbar-dark navbar-static-top">
-<a class="navbar-brand" href="index.php">e-Eyes</a>
+<a class="navbar-brand" href="./index.php">e-Eyes</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="collapsibleNavbar">
 <ul class="navbar-nav">
 <li class="nav-item">
-<a class="nav-link" href="index.php">HOME</a>
+<a class="nav-link" href="./index.php">HOME</a>
 </li>
 <li class="nav-item">
-<a class="nav-link active" href="about.php">ABOUT</a>
+<a class="nav-link active" href="./about.php">ABOUT</a>
 </li>
 </ul>
 <ul class="navbar-nav ml-auto">
 <li class="nav-item">
-<a class="nav-link" href="#"><img src="logout.png" width=20px height=20px></img>LOGOUT</a>
+<a class="nav-link" href="./auth/logout.php"><img src="./images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
 </li>
 </ul>
 </div>
@@ -45,10 +45,10 @@
   <p>This website is the solution</p>
 </div>
 
-<h2 style="text-align:center">Our Team</h2>
-<div class="cards-wrapper">
+<h2 style="text-align:center" class="mt-5">Our Team</h2>
+<div class="cards-wrapper mb-5">
     <div class="card bg-info" style="width: 250px">
-      <img src="profile.png" alt="Felix Filipi" style="width:100%">
+      <img src="./images/resources/Felix.jpeg" alt="Felix Filipi" style="width:100%">
       <div class="container">
         <h4>Felix Filipi</h4>
         <p class="title">Project Manager & Machine Learning</p>
@@ -58,23 +58,22 @@
 
 
     <div class="card bg-info" style="width: 250px">
-      <img src="profile.png" alt="Febrian Nugroho" style="width:100%">
+      <img src="./images/resources/Febrian.jpeg" alt="Febrian Nugroho" style="width:100%">
       <div class="container">
-        <h4>Felix Nugroho</h4>
+        <h4>Febrian Nugroho</h4>
         <p class="title">Backend Developer</p>
         <p>A man who wants to be loved.</p>
       </div>
     </div>
   
     <div class="card bg-info" style="width: 250px">
-      <img src="profile.png" alt="Christoper Luis Alexander" style="width:100%">
+      <img src="./images/resources/Christoper.jpg" alt="Christoper Luis Alexander" style="width:100%">
       <div class="container">
         <h4>Christoper Luis Alexander</h4>
         <p class="title">Frontend Developer</p>
         <p>A man who wants to be loved.</p>
       </div>
     </div>
-
 </div>
 
 <div class="jumbotron text-center text-white bg-dark" style="margin-bottom:0">
