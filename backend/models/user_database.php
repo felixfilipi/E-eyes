d088dbe1e27bc9f727d73fa0b19e1f954ca2dace
Christoper Luis Alexander <59040138+Christoper0704@users.noreply.github.com>
2021-08-14 19:54:35 +0700

    Update user_database.php

--- backend/models/user_database.php
+++ backend/models/user_database.php
@@ -20,33 +20,33 @@ require_once "./functions.php";
 <meta name="viewport" content="width-device-width initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/style1.css">
+<link rel="stylesheet" href="../styles/style1.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-<script src="js/index.js"></script>
+<script src="../styles/index.js"></script>
 </head>
 
 <body>
 <nav class="navbar navbar-expand-sm bg-info navbar-dark navbar-static-top">
-<a class="navbar-brand" href="index.php">e-Eyes</a>
+<a class="navbar-brand" href="../index.php">e-Eyes</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="collapsibleNavbar">
 <ul class="navbar-nav">
 <li class="nav-item">
-<a class="nav-link" href="index.php">HOME</a>
+<a class="nav-link" href="../index.php">HOME</a>
 </li>
 <li class="nav-item">
-<a class="nav-link" href="about.php">ABOUT</a>
+<a class="nav-link" href="../about.php">ABOUT</a>
 </li>
 </ul>
 <ul class="navbar-nav ml-auto">
 <li class="nav-item">
-<a class="nav-link" href="#"><img src="logout.png" width=20px height=20px></img>LOGOUT</a>
+<a class="nav-link" href="../auth/logout.php"><img src="../images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
 </li>
 </ul>
 </div>
@@ -129,10 +129,6 @@ echo "0 results";
 <div class="jumbotron text-center text-white bg-dark" style="margin-bottom:0">
   <p>Copyright ©2021</p>
 </div>
-
-<script>
-
-</script>
 
 </body>
 </html>
