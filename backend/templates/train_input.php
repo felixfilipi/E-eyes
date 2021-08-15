7ed49651e35c98eea5b7de97a316792bf7e5b0de
Christoper Luis Alexander <59040138+Christoper0704@users.noreply.github.com>
2021-08-15 21:18:27 +0700

    Update train_input.php

--- backend/templates/train_input.php
+++ backend/templates/train_input.php
@@ -14,27 +14,29 @@
 		<script src="../styles/index.js"></script>
     </head>
 <body>
-	<nav class="navbar navbar-expand-sm bg-info navbar-dark navbar-static-top">
-	<a class="navbar-brand" href="../index.php">e-Eyes</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-	<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="collapsibleNavbar">
-	<ul class="navbar-nav">
-	<li class="nav-item">
-	<a class="nav-link" href="../index.php">HOME</a>
-	</li>
-	<li class="nav-item">
-	<a class="nav-link" href="../about.php">ABOUT</a>
-	</li>
-	</ul>
-	<ul class="navbar-nav ml-auto">
-	<li class="nav-item">
-	<a class="nav-link" href="../auth/logout.php"><img src="../images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
-	</li>
-	</ul>
-	</div>
-	</nav>
+<script src="../styles/logoutfunc.js"> </script>
+
+<nav class="navbar navbar-expand-sm navbar-dark navbar-static-top" style="background: #356BF3;">
+<a class="navbar-brand" href="../index.php"><img src="../images/resources/coole.png" alt="logo" style="width:60px;"></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="../index.php">HOME</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="../about.php">ABOUT</a>
+</li>
+</ul>
+<ul class="navbar-nav ml-auto">
+<li class="nav-item">
+<a class="nav-link" id="logout" href="../auth/logout.php" onclick="logout"><img src="../images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
+</li>
+</ul>
+</div>
+</nav>
 	
     <div class="container mt-5">
   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method = "post">
@@ -74,14 +76,15 @@
 		<span class="help-block"><?php echo $role_err ?></span>
 	</div>
   </div>
+  <br>
   <button type="submit" class="btn btn-success mb-2 mt-2">Submit</button>
   <button type="reset" class="btn btn-danger ml-2 mb-2 mt-2">Cancel</button>
   </form>
 </div>
 
 
-
 <div class="jumbotron text-center text-white bg-dark" style="margin-bottom:0">
+  <p>Cool-e Incorporation</p>
   <p>Copyright ©2021</p>
 </div>
 
