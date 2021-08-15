b19bdfb2f0efcc1f7aa2634dd6c45e13d396b7c2
Christoper Luis Alexander <59040138+Christoper0704@users.noreply.github.com>
2021-08-15 21:18:53 +0700

    Update camerastream.php

--- backend/models/camerastream.php
+++ backend/models/camerastream.php
@@ -7,7 +7,7 @@
 <meta name="viewport" content="width-device-width initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../styles/style1.css">
+<link rel="stylesheet" href="../styles/style2.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -17,8 +17,10 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-sm bg-info navbar-dark navbar-static-top">
-<a class="navbar-brand" href="../index.php">e-Eyes</a>
+<script src="../styles/logoutfunc.js"> </script>
+
+<nav class="navbar navbar-expand-sm navbar-dark navbar-static-top" style="background: #356BF3;">
+<a class="navbar-brand" href="../index.php"><img src="../images/resources/coole.png" alt="logo" style="width:60px;"></a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 <span class="navbar-toggler-icon"></span>
 </button>
@@ -33,49 +35,26 @@
 </ul>
 <ul class="navbar-nav ml-auto">
 <li class="nav-item">
-<a class="nav-link" href="../auth/logout.php"><img src="../images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
+<a class="nav-link" id="logout" href="../auth/logout.php" onclick="logout"><img src="../images/resources/logout.png" width=20px height=20px></img>LOGOUT</a>
 </li>
 </ul>
 </div>
 </nav>
 
 <div class="container">
-  <div class="mt-5"><img src="../images/resources/techbg.jpg" width=100% height=450px></img></div>
-  </div>
+
+</div>
 
 <div class="container">
-<div class="text-dark bg-light"><button class="btn-outline-light" id="startrecord" onclick="startrecord()"><img src="../images/resources/record.png" height="60px" width="60px" center></img></button></div>
+<div class="text-dark bg-dark"><button class="btn-outline-light" id="startrecord"><img src="../images/resources/record2.png" height="60px" width="60px" center></img></button></div>
 </div>
+
+<br><br>
 
 <div class="jumbotron text-center text-white bg-dark" style="margin-bottom:0">
+  <p>Cool-e Incorporation</p>
   <p>Copyright ©2021</p>
 </div>
-
-<script>
-  function startrecord(){
-	  let recorder = new MediaRecorder(stream);
-	  let data = [];
-
-	  recorder.ondataavailable = event => data.push(event.data);
-	  recorder.start();
-	  log(recorder.state + " for " + (lengthInMS/1000) + " seconds...");
-
-	  let stopped = new Promise((resolve, reject) => {
-		recorder.onstop = resolve;
-		recorder.onerror = event => reject(event.name);
-	  });
-
-	  let recorded = wait(lengthInMS).then(
-		() => recorder.state == "recording" && recorder.stop()
-	  );
-
-	  return Promise.all([
-		stopped,
-		recorded
-	  ])
-	  .then(() => data);
-  }
-</script>
 
 </body>
 </html>
