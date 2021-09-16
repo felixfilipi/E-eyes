779d498856d834ca57a7d133347c8599d52ad85e
Felix Filipi <65327805+felixfilipi@users.noreply.github.com>
2021-09-16 20:13:26 +0700

    Update about.php

--- website/about.php
+++ website/about.php
@@ -5,6 +5,7 @@
 <title>e-Eyes</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width-device-width initial-scale=1">
+<meta name="dicoding:email" content="felixfilipi4@gmail.com">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="./styles/style1.css">
